047b4ed326edd3168893c8a11955277c17c1352a
Hugo Leone de Araújo Souza <hugoleone.dev@gmail.com>
2019-05-13 11:28:47 -0400

    Copyright

--- views/footer.php
+++ views/footer.php
@@ -2,7 +2,7 @@
 		<div class="rodape">
 			<img src="../img/rodape.jpg">
 			<div class="col-md-12">
-					<p>&copy; 2019 Hello, World! | Todos os Direitos Reservados. Construido por <a href="#">DST</a>.</p>
+					<p>&copy; Copyright 2019</p>
 				</div>
 		</div>			
 
